8f26375c7389067b09c63889d61730b7dba29e63
yuxi.dong <yuxi.dong@unisoc.com>
2019-12-20 09:58:40 +0800

    Bug #1218819 add vdsp nnhal service
    [root cause  ] add vdsp nnhal service
    [changes     ] add
    [side effects] n/a
    [self test   ]: ok
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: n/a
    [confirm dependent commit]: yes
    [board]: sharkl5Pro
    [test case]: vdsp
    [reviewers   ] kevin.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    Change-Id: I03acd7ac8188b64a66d3345ca834e33941756a2d

---
+++ Cadence/nnhal/device-vdsp-nnhal.mk
@@ -0,0 +1,5 @@
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/android.hardware.neuralnetworks@1.2-service-xtensa.rc:/vendor/etc/init/android.hardware.neuralnetworks@1.2-service-xtensa.rc
+
+PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.2-service-xtensa \

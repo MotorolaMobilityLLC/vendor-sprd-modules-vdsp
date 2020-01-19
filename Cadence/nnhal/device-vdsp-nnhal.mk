f024636ef4dc057e6add9bffe6170d20651fbfa2
yuxi.dong <yuxi.dong@unisoc.com>
2020-01-19 16:15:26 +0800

    Bug #1247804 close ann for cadence's bad code
    [root cause  ] close ann for cadence's bad code
    [changes     ] add
    [side effects] n/a
    [self test   ]: ok
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: yes
    [is there dependence]: n/a
    [confirm dependent commit]: yes
    [board]: sharkl5Pro
    [test case]: vdsp
    [reviewers   ] kevin.yang
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] specific
    
    Change-Id: Ib83f10b613faa02a7969c9ca34228eb8ae50e348

--- Cadence/nnhal/device-vdsp-nnhal.mk
+++ Cadence/nnhal/device-vdsp-nnhal.mk
@@ -1,5 +1,1 @@
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/android.hardware.neuralnetworks@1.2-service-xtensa.rc:/vendor/etc/init/android.hardware.neuralnetworks@1.2-service-xtensa.rc
 
-PRODUCT_PACKAGES += \
-    android.hardware.neuralnetworks@1.2-service-xtensa \

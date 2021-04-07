c8252cda3a7268db6cbc18f631e0d5606dc7ed2e
huiwen.wei <huiwen.wei@unisoc.com>
2021-04-07 19:39:18 +0800

    Bug #1579666 rm systest dir
    [root cause  ] rm systest dir
    [changes     ] del
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums9620
    [test case]:vdsp
    [reviewers   ] yongchao.tang
    [change_type ] feature
    [tag_product ] common
    
    Change-Id: Icd38925a84071648e406fcea9a8805d38f30b07d

--- Cadence/xrp/products/sharkl5pro/vdsp_service.mk
+++ Cadence/xrp/products/sharkl5pro/vdsp_service.mk
@@ -4,8 +4,6 @@ PRODUCT_PACKAGES += libvdspservice \
 PRODUCT_COPY_FILES += \
     vendor/sprd/modules/vdsp/Cadence/xrp/init.sprd_vdsp.rc:vendor/etc/init/init.sprd_vdsp.rc \
 	vendor/sprd/modules/vdsp/Cadence/xrp/xrp-firmware/sharkl5pro/vdsp_firmware.bin:vendor/firmware/vdsp_firmware.bin \
-	vendor/sprd/modules/vdsp/Cadence/xrp/xrp-firmware/sharkl5pro/test_lib.o:/vendor/lib/test_lib.o \
-	vendor/sprd/modules/vdsp/Cadence/xrp/xrp-firmware/sharkl5pro/test_lib.bin:vendor/firmware/test_lib.bin \
 	vendor/sprd/modules/vdsp/Cadence/xrp/xrp-firmware/sharkl5pro/faceid_fw.bin:vendor/firmware/faceid_fw.bin \
 	vendor/sprd/modules/vdsp/Cadence/xrp/xrp-firmware/sharkl5pro/network_coeff_fd_o.bin:vendor/firmware/network_coeff_fd_o.bin \
 	vendor/sprd/modules/vdsp/Cadence/xrp/xrp-firmware/sharkl5pro/network_coeff_fd_p.bin:vendor/firmware/network_coeff_fd_p.bin \
